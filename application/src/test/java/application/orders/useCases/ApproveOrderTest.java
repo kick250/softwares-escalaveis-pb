4b51711201223b81117c3b437cf11922c4a8d0b2
kick250 <kicklobat@gmail.com>
2025-10-13 21:56:35 -0300

    Adicionar jacoco

---
+++ application/src/test/java/application/orders/useCases/ApproveOrderTest.java
@@ -0,0 +1,11 @@
+package application.orders.useCases;
+
+import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
+
+public class ApproveOrderTest extends TestCase {
+    @Test
+    public void testApproveOrder() {
+        assertEquals(true, true);
+    }
+}
---
+++ infra/src/test/java/infra/orders/repositories/OrdersJpaRepositoryTest.java
@@ -0,0 +1,13 @@
+package infra.orders.repositories;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class OrdersJpaRepositoryTest {
+
+    @Test
+    public void testOrdersJpaRepository() {
+        assertEquals(true, true);
+    }
+}
